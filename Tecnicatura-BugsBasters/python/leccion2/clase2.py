f4c035e4f8a694403d6ec9d9a41767dad35b4914
Luu102 <lourdeseschler@gmail.com>
2025-06-05 19:18:45 -0300

    Leccion 2 de python

---
+++ Tecnicatura-BugsBasters/python/leccion2/clase2.py
@@ -0,0 +1,14 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los estudiantes de la tecnicatura"
+print(miVariable)
+miVariable= 3.5
+print(miVariable)
+x = 10
+y= 2
+z = x + y
+print(z)
+print(id(x))
+#las literales se escriben x528.por ej.
+print(id(y))
+print(id(z))
