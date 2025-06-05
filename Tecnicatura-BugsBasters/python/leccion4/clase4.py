ef7dfd33e6b4b42281caeb7be4abb40ad49de631
Luu102 <lourdeseschler@gmail.com>
2025-06-05 19:20:29 -0300

    Leccion 4 de python

---
+++ Tecnicatura-BugsBasters/python/leccion4/clase4.py
@@ -0,0 +1,6 @@
+alto =int(input("Proporcione el alto del rectangulo: "))
+ancho=int(input("Proporcione el ancho del rectangulo: "))
+area = alto*ancho
+perimetro = (alto+ancho) * 2
+print("area: ",area)
+print("permetro: ", perimetro)
---
+++ Tecnicatura-BugsBasters/python/leccion4/clase4ejercicio2.py
@@ -0,0 +1,6 @@
+edadAdulto =18
+edadPersona=int(input("Ingrese su edad: "))
+if edadPersona >= edadAdulto:
+    print(f"su edad es: {edadPersona} es mayor de edad")
+else:
+    print(f"su edad es: {edadPersona} es menor de edad")
