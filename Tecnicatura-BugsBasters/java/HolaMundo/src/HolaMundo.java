dd386d2a25ba4bafb70057da27faf9aec48b1cea
Luu102 <lourdeseschler@gmail.com>
2025-06-05 21:19:35 -0300

    HolaMundo

---
+++ Tecnicatura-BugsBasters/java/HolaMundo/src/HolaMundo.java
@@ -0,0 +1,14 @@
+//Nuestro primer pyocto de Java
+/*Para varias lineas de comentarios
+y muchas mas
+y mas
+*/
+
+package holamundo.pkg2;
+
+import java.util.scanner;
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+    }
+}
---
+++ Tecnicatura-BugsBasters/java/HolaMundo/src/Tipos_primitivos/Tipos_primitivos.java
@@ -0,0 +1,19 @@
+
+
+package Tipos_primitivos;
+
+public class Tipos_primitivos {
+    public static void main(String[] args) {
+        
+        int miVariable =10;
+        System.out.println(miVariable);
+        miVariable =5;
+        System.out.println(miVariable);
+        //tipo string
+        String miVariableCadena ="Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena="Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+    
+}
