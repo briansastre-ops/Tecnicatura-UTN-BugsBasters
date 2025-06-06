69edac47fd649b1330ca5276cc36d48753ff35fa
Luu102 <lourdeseschler@gmail.com>
2025-06-06 11:52:11 -0300

    Leccion 8 de python

---
+++ Tecnicatura-BugsBasters/python/leccion8/ejerciciosWhile.py
@@ -0,0 +1,48 @@
+#Ciclo while (Mientras)
+'''
+contador = 0
+while contador < 3
+    print("Ejecutamos nuestro ciclo while ", contador)
+    contador += 1
+else:
+    print("Fin del ciclo while")'''
+'''
+#Imprimir numeros del 0 al 5 con el ciclo while
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(contador)
+    contador += 1'''
+'''
+minimo = 1
+contador = 5
+while contador >= minimo:
+    print(contador)
+    contador -= 1'''
+
+#Ciclo For (Para)
+'''
+cadena = "Hello"
+for letra in cadena:
+    print(letra)
+else:
+    print("Fin del ciclo for")'''
+
+#Palabra reservada Break
+''''
+for letra in "Alemania":
+    if letra == "a":
+        print(f"Letra encontrada: {letra}")
+        break
+else:
+        print("Fin del ciclo for")'''
+
+#Palabra reservada Continue
+for i in range(6):
+    if i % 2 == 0:
+        print(f"Valor: {i}")
+
+for i in range(6):
+    if i % 2 != 0:
+        continue
+    print(f"Valor: {i}")
